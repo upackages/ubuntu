e3168861e181c99a7c08df444a4943445747b603
Joshua <jqshuv@gmail.com>
2022-05-02 19:17:44 +0200

    Initial Test

---
+++ apply.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+render() {
+sedStr="
+  s!%%UBUNTU_VERSION%%!$version!g;
+  s!%%MAINTAINER%%!$maintainer!g;
+  s!%%REPOSITORY%%!$repository!g;
+"
+
+sed -r "$sedStr" $1
+}
+
+maintainer=(hello@unately.com)
+versions=(bionic focal impish jammy kinetic trusty xenial latest)
+repository=(https://github.com/upackages/ubuntu)
+
+for version in ${versions[*]}; do
+  mkdir $version
+  render Dockerfile.template > $version/Dockerfile
+done
